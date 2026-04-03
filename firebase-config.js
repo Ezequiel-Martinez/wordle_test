4b6d4bf01e0d31c954530ae37178fdc98fd10094
Ezequiel <zezequielmartinez@gmail.com>
2026-04-03 02:30:35 -0300

    seguridad cambios

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyBJTWudIZOvbTddc938i_4eHBCJ_7K1usY",
+  authDomain: "curiosity-vs-utility.firebaseapp.com",
+  databaseURL: "https://curiosity-vs-utility-default-rtdb.firebaseio.com/",
+  projectId: "curiosity-vs-utility",
+  storageBucket: "curiosity-vs-utility.firebasestorage.app",
+  messagingSenderId: "927110688695",
+  appId: "1:927110688695:web:9e5443d98b7400d1118232",
+};
